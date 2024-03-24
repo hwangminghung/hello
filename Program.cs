e4062945dc9f7d56c1498d982ef0d78d7fc55ced
hwangminghung <hwangminghung@gmail.com>
2024-03-24 15:16:01 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Nhập tên của bạn: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Xin chào, " + name + "!");
+        
+    }
+}
